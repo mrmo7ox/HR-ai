fe70b0638198de8b9877c9a14c5d4d1ecfe898f0
zakaria <zaki.guellouch@gmail.com>
2025-10-24 15:26:12 +0100

    first commit

---
+++ front/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className=''>
+      <h1>What can I help with concerning HR?</h1>
+
+      <input type="text" placeholder='Ask anything' />
+    </div>
+  )
+}
+
+export default App
